14032d80fc6d14291f67afdb1a552226ce3899ef
costar011 <dnaer456@gmail.com>
2023-01-02 21:51:23 +0900

    coding test

--- Day3/2.c
+++ Day3/2.c
@@ -1,4 +1,6 @@
 /*
+문제 설명
+
 중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다.
 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다.
 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
@@ -31,5 +33,16 @@ array	                result
 // array_len은 배열 array의 길이입니다.
 int solution(int array[], size_t array_len) {
     int answer = 0;
-    return answer;
+    int i,j,temp = 0;
+    
+    for(i = 0; i < array_len; i++){
+        for(j = 0; j < (array_len - 1) - i; j++) {
+            if(array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return answer = array[array_len/2];
 }
